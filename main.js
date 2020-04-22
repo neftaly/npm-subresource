3a8e75541b422dc5a392bb296a9a6bc85a68014b
XhmikosR <xhmikosr@gmail.com>
2020-04-22 12:29:18 +0300

    ES6-ify

--- main.js
+++ main.js
@@ -6,29 +6,26 @@
 
 "use strict";
 
-var fs = require("fs"),
-    path = require("path"),
-    sri = require("sri-toolbox").generate,
-    R = require("ramda"),
+const fs = require("fs");
+const path = require("path");
+const sri = require("sri-toolbox").generate;
+const R = require("ramda");
 
-    generate,
-    defaults;
-
-defaults = {
+const defaults = {
     algorithms: ["sha256"],
     full: true
 };
 
 // Load file, and build SRI
-generate = R.memoize(function (filePath, options) {
+const generate = R.memoize((filePath, options) => {
     options = R.merge(defaults, options);
-    var data = fs.readFileSync(filePath);
+    const data = fs.readFileSync(filePath);
     return sri(options, data);
 });
 
 
 // Resolve file path, and request SRI
-module.exports = R.memoize(function (filePath, options) {
+module.exports = R.memoize((filePath, options) => {
     filePath = path.resolve(filePath);
     return generate(filePath, options);
 });
--- test/tests.js
+++ test/tests.js
@@ -1,55 +1,55 @@
 "use strict";
 
-var assert = require("assert"),
-    path = require("path"),
-    subresource = require(".."),
-    fs = require("fs-sync");
+const assert = require("assert");
+const path = require("path");
+const subresource = require("..");
+const fs = require("fs-sync");
 
-var tempFixture = path.join(__dirname, "./fixtures/temp/jquery-1.10.2.min.js");
+const tempFixture = path.join(__dirname, "./fixtures/temp/jquery-1.10.2.min.js");
 
-describe("Generate:", function () {
-    beforeEach(function (done) {
+describe("Generate:", () => {
+    beforeEach(done => {
         fs.copy(path.join(__dirname, "./fixtures/jquery-1.10.2.min.js"), tempFixture);
         done();
     });
 
-    describe("sha256:", function () {
-        var expect = {
+    describe("sha256:", () => {
+        const expect = {
             hashes: {
                 sha256: "C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg="
             },
             integrity: "sha256-C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg="
         };
 
-        it("Initial hit", function () {
-            var result = subresource(tempFixture);
+        it("Initial hit", () => {
+            const result = subresource(tempFixture);
             assert.deepEqual(expect, result);
         });
 
-        it("Cached hit", function () {
+        it("Cached hit", () => {
             fs.remove(tempFixture); // Make sure it can't re-load the fixture
-            var result = subresource(tempFixture);
+            const result = subresource(tempFixture);
             assert.deepEqual(expect, result);
         });
     });
 
-    describe("sha384:", function () {
-        var options = { algorithms: ["sha384"] };
-        var expect = {
+    describe("sha384:", () => {
+        const options = { algorithms: ["sha384"] };
+        const expect = {
             hashes: {
                 sha384: "hK8q2gkBjirpIGHAH+sgqYMv6i6mfx2JVZWJ50jyYhkuEHASU6AS1UTWSo32wuGL"
             },
             integrity: "sha384-hK8q2gkBjirpIGHAH+sgqYMv6i6mfx2JVZWJ50jyYhkuEHASU6AS1UTWSo32wuGL"
         };
 
-        it("Initial hit", function () {
-            var result = subresource(tempFixture, options);
+        it("Initial hit", () => {
+            const result = subresource(tempFixture, options);
             assert.deepEqual(expect, result);
         });
 
-        it("Cached hit", function () {
+        it("Cached hit", () => {
             fs.remove(tempFixture); // Make sure it can't re-load the fixture
-            var result = subresource(tempFixture, options);
+            const result = subresource(tempFixture, options);
             assert.deepEqual(expect, result);
         });
     });
